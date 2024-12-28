31448d89eedf16ca548daf02e212674e11c1847a
mahdikhashan <mahdikhashan1@gmail.com>
2024-12-28 16:03:07 +0100

    wip: ip-service

--- ip/ip.go
+++ ip/ip.go
@@ -1,8 +1,32 @@
 package ip
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func IpHello(name string) string {
+type ip struct {
+	Host string `json:"host"`
+}
+
+var hostIP = ip{Host: "1.2.3.4"}
+
+func getIP(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, hostIP)
+}
+
+func Hello(name string) string {
 	message := fmt.Sprintf("Hi. %v !", name)
 	return message
+}
+
+func StartService() {
+	router := gin.Default()
+	router.GET("/", getIP)
+
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"service/ip"
 )
 
 func main() {
-	message := ip.IpHello("mahdi")
-	fmt.Println(message)
+	ip.StartService()
 }
