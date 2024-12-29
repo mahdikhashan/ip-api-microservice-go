ed68ef3ab8cd1a7c5d1f0e810db038b51a42773f
mahdikhashan <mahdikhashan1@gmail.com>
2024-12-29 14:53:08 +0100

    wip: ip-service - custom logging - log headers

--- ip/ip.go
+++ ip/ip.go
@@ -1,12 +1,14 @@
 package ip
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	healthcheck "github.com/tavsec/gin-healthcheck"
 	"github.com/tavsec/gin-healthcheck/checks"
 	"github.com/tavsec/gin-healthcheck/config"
 	"net/http"
 	"os"
+	"time"
 )
 
 type ip struct {
@@ -29,9 +31,21 @@ func getIP(c *gin.Context) {
 func StartService() {
 	router := gin.Default()
 	router.GET("/", getIP)
-
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.Header,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
 	healthcheck.New(router, config.DefaultConfig(), []checks.Check{})
-
 	err := router.Run("0.0.0.0:" + PORT)
 	if err != nil {
 		return
